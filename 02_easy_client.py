9729d74e965a036450f07b940e3ddda16b57c3b4
yoggy <yoggy0@gmail.com>
2013-07-21 18:51:07 +0900

    modified:   02_easy_client.py, easy_tcp.py

--- 02_easy_client.py
+++ 02_easy_client.py
@@ -1,17 +1,12 @@
 #!/usr/bin/python
 
-# for easy_tcp
-from easy_tcp import connect
-from easy_tcp import send
-from easy_tcp import recv
-from easy_tcp import close
+import easy_tcp
 
 # connect to server
-connect("::1", 12345)
+s = easy_tcp.connect("::1", 12345)
 
 # read message from server (1.0 is read_timeout)
-msg = recv(1.0) 
+msg = s.recv(1.0) 
+s.send("123")
 
-send("123")
-
-close()
+s.close()
--- easy_tcp.py
+++ easy_tcp.py
@@ -5,11 +5,6 @@ import select
 # log setting
 easy_tcp_output_log_level = 0
 
-# global object...
-easy_tcp_host_   = ""
-easy_tcp_port_   = -1
-easy_tcp_socket_ = None
-
 def is_disable_output(my_level):
 	global easy_tcp_output_log_level
 	if my_level < easy_tcp_output_log_level:
@@ -28,15 +23,7 @@ def log_d(name, msg):
 	if is_disable_output(0): return
 	print "easy_tcp[D] %s() : %s" % (name, msg)
 
-def socket_check():
-	global easy_tcp_socket_
-	if easy_tcp_socket_ is None:
-		log_e("recv", "please connect() first!. socket is None...")
-		sys.exit(1)
-
 def connect(host, port):
-	global easy_tcp_host_, easy_tcp_port_, easy_tcp_socket_
-
 	log_d("connect", "host=%s, port=%d" % (host, port))
 
 	res = socket.getaddrinfo(host, str(port))
@@ -67,47 +54,43 @@ def connect(host, port):
 
 	s.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
 
-	easy_tcp_host_ = host
-	easy_tcp_port_ = port
-	easy_tcp_socket_ = s
-
+	tcp = EasyTCP(s, host, port)
 	log_i("connect", "connect() success...host=%s, port=%d" % (host, port))
+	return tcp
 	
-def close():
-	global easy_tcp_host_, easy_tcp_port_, easy_tcp_socket_
+class EasyTCP:
+	def __init__(self, socket, host, port):
+		self.socket = socket
+		self.host   = host
+		self.port   = port
 
-	if easy_tcp_socket_ is not None:
-		easy_tcp_socket_.close()
-		easy_tcp_socket_ = None
-		log_i("close", "close socket...host=%s, port=%d" % (easy_tcp_host_, easy_tcp_port_))
-		easy_tcp_host_ = ""
-		easy_tcp_port_ = -1
-
-def send(data):
-	global easy_tcp_socket_
-	socket_check()
-
-	log_d("send", "data=%s" % data)
-	easy_tcp_socket_.sendall(data)
-
-def recv(read_timeout=0.5):
-	global easy_tcp_socket_
-	socket_check()
-
-	easy_tcp_socket_.setblocking(0)
+	def send(self, data):
+		log_d("send", "data=%s" % data)
+		self.socket.sendall(data)
 	
-	data = ""
-
-	while True:
-		r = select.select([easy_tcp_socket_], [], [], read_timeout)
-		if r[0]:
-			d = easy_tcp_socket_.recv(1024)
-			if len(d) > 0:
-				data += d
+	def recv(self, read_timeout=0.5):
+		self.socket.setblocking(0)
+		
+		data = ""
+	
+		while True:
+			r = select.select([self.socket], [], [], read_timeout)
+			if r[0]:
+				d = self.socket.recv(1024)
+				if len(d) > 0:
+					data += d
+				else:
+					break
 			else:
 				break
-		else:
-			break
-	log_d("recv", "data=%s" % data)
-	return data
-	
+		log_d("recv", "data=%s" % data)
+		return data
+
+	def close(self):
+		if self.socket is not None:
+			self.socket.close()
+			self.socket = None
+			log_i("close", "close socket...host=%s, port=%d" % (self.host, self.port))
+			self.host = ""
+			self.port = -1
+
