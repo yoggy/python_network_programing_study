10103c2ff4ece2c53531ed3da3b65d123039d5bb
yoggy <yoggy0@gmail.com>
2013-07-22 16:00:19 +0900

    modified:   02_easy_client.py, easy_tcp.py

--- 02_easy_client.py
+++ 02_easy_client.py
@@ -3,10 +3,32 @@
 import easy_tcp
 
 # connect to server
-s = easy_tcp.connect("::1", 12345)
+socket = easy_tcp.connect("::1", 12345)
 
-# read message from server (1.0 is read_timeout)
-msg = s.recv(1.0) 
-s.send("123")
+while True:
+	# read message from server
+	msg = socket.recv()
+	
+	# create answer string
+	for l in msg.splitlines():
+		# find "numbers : 1 2 3" line
+		if "numbers :" in l:
+	
+			# split numbers
+			numstr = l.split(":")[1].strip().split(" ")
+			num = [int(s) for s in numstr]
+	
+			# sort numbers & create answer string
+			ans = sorted(num)
+			ansstr = " ".join([str(n) for n in ans])
+			break
 
-s.close()
+		# if you got key string
+		if "congraturation" in l:
+			key = l.split("=")[1].strip()
+			print "!!!!!!!! key is %s !!!!!!!!" % key
+			exit(0)
+	
+	# send anser
+	socket.sendln(ansstr)
+	
--- easy_tcp.py
+++ easy_tcp.py
@@ -29,6 +29,7 @@ def connect(host, port):
 	res = socket.getaddrinfo(host, str(port))
 
 	s = None
+	err = ""
 	for (ai_family, ai_socket_type, ai_protocol, ai_canonname, ai_addr) in res:
 		try:
 			# check addrinfo
@@ -45,11 +46,11 @@ def connect(host, port):
 			s.connect((ai_addr[0], ai_addr[1]))
 			break
 		except Exception as e:
-			print e
+			err = e
 			s = None
 
 	if s is None:
-		log_e("connect", "connect() failed...host=%s, port=%d" % (host, port))
+		log_e("connect", "connect() failed...err=%s" % err)
 		sys.exit(1)
 
 	s.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
@@ -64,33 +65,60 @@ class EasyTCP:
 		self.host   = host
 		self.port   = port
 
+	def __del__(self):
+		self.close()
+
 	def send(self, data):
 		log_d("send", "data=%s" % data)
-		self.socket.sendall(data)
+		try:
+			self.socket.sendall(data)
+		except Exception as e:
+			log_e("send", "send() failed...err=%s" % e)
+			sys.exit(1)
+
+	def sendln(self, data):
+		data = data + "\r\n"
+		self.send(data)
 	
 	def recv(self, read_timeout=0.5):
 		self.socket.setblocking(0)
 		
 		data = ""
+
+		is_first = True;
 	
-		while True:
-			r = select.select([self.socket], [], [], read_timeout)
-			if r[0]:
-				d = self.socket.recv(1024)
-				if len(d) > 0:
-					data += d
+		try:
+			while True:
+				t = read_timeout
+				if is_first: t = t * 10.0
+
+				r = select.select([self.socket], [], [], t)
+
+				is_first = False
+
+				if r[0]:
+					d = self.socket.recv(1024)
+					if len(d) > 0:
+						data += d
+					else:
+						break
 				else:
 					break
-			else:
-				break
-		log_d("recv", "data=%s" % data)
+		except Exception as e:
+			log_e("recv", "recv() failed...err=%s" % e)
+			sys.exit(1)
+			
+		log_d("recv", "size=%d, data=%s" % (len(data), data))
 		return data
 
 	def close(self):
-		if self.socket is not None:
-			self.socket.close()
-			self.socket = None
-			log_i("close", "close socket...host=%s, port=%d" % (self.host, self.port))
-			self.host = ""
-			self.port = -1
+		try:
+			if self.socket is not None:
+				self.socket.close()
+				self.socket = None
+				log_i("close", "close socket...host=%s, port=%d" % (self.host, self.port))
+				self.host = ""
+				self.port = -1
+		finally:
+			pass
 
